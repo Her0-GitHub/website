99252557bd6b8816fe85c566c84e1c407b71171e
Her0_ <79155428+Her0-GitHub@users.noreply.github.com>
2023-03-04 00:50:43 +0000

    Bug Fix and Update 03/03

--- robaScuola/15Game/js/game.js
+++ robaScuola/15Game/js/game.js
@@ -5,16 +5,36 @@ for (let i = 0; i < board.length; i++) {
     board[i].addEventListener("click", move);
 }
 
-idEmpty = "box-16";
+let first = true;
+let idEmpty = "box-16";
+
 function move(){
-    empty = document.getElementById(idEmpty);
+    let empty = document.getElementById(idEmpty);
     dif  = Math.abs(parseInt(this.id.slice(4)) - parseInt(empty.id.slice(4)));
     if(this.innerHTML != '' &&  (dif == 4 || dif == 1)){
+        // add class for css
         this.className = 'box empty';
         empty.className = 'box'
+        // swap number
         temp = this.innerHTML;
         this.innerHTML = empty.innerHTML;
         empty.innerHTML =  temp;
+        // change id empty var
         idEmpty = this.id;
+        // check win only when empty is in box-16
+        if(this.id == "box-16"){
+            checkWin();
+        }
+        if(first)  startCronometro(); first = false;
+    }   
+}
+
+function checkWin(){
+    for (let i = 1; i < board.length; i++) {
+        if(document.getElementById("box-" + i).innerHTML != i.toString()){
+            return;
+        }
     }
+    stopCronometro();
+    showDialog(`15 Game`, "Tempo Totale: " + (document.getElementById("cronometro").innerText).replaceAll("\n", ""), "Gioca");
 }
