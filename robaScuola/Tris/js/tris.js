2fcb78ea744d3d507ffb199122358262fd746608
Her0_ <79155428+Her0-GitHub@users.noreply.github.com>
2023-05-19 18:26:44 +0000

    Revert "js tris update"
    This reverts commit 9c63e951e24e9073a3b7d20f38b032c77adeb74f.

--- robaScuola/Tris/js/tris.js
+++ robaScuola/Tris/js/tris.js
@@ -7,11 +7,11 @@ for (i = 0; i < board.length; i++) {
 function input() {
     if (this.innerHTML == '') {
         nTurni++;
-        this.innerHTML = turno;
+        document.getElementById(this.id).innerHTML = turno;
         if(win(this.id))            return;
         else if(nTurni == 9)        showDialog(`Pareggio`, '', 'Gioca Ancora');
     } else return;
-
+    
     if (turno == "X")   turno = "O";
     else                turno = "X";
 }
@@ -19,11 +19,11 @@ function win(add) {
     if( (document.getElementById("a1").innerHTML == turno && document.getElementById("a2").innerHTML == turno && document.getElementById("a3").innerHTML == turno) ||  // A-A-A-
         (document.getElementById("b1").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("b3").innerHTML == turno) ||  // B-B-B-
         (document.getElementById("c1").innerHTML == turno && document.getElementById("c2").innerHTML == turno && document.getElementById("c3").innerHTML == turno) ||  // C-C-C-
-        (document.getElementById("a1").innerHTML == turno && document.getElementById("b1").innerHTML == turno && document.getElementById("c1").innerHTML == turno) ||  // 1|1|1|
-        (document.getElementById("a2").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c2").innerHTML == turno) ||  // 2|2|2|
-        (document.getElementById("a3").innerHTML == turno && document.getElementById("b3").innerHTML == turno && document.getElementById("c3").innerHTML == turno) ||  // 3|3|3|
-        (document.getElementById("a1").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c3").innerHTML == turno) ||  // A1\B2\C3\
-        (document.getElementById("a3").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c1").innerHTML == turno)     // C1/B2/A3/
+        (document.getElementById("a1").innerHTML == turno && document.getElementById("b1").innerHTML == turno && document.getElementById("c1").innerHTML == turno) ||  // A|A|A|
+        (document.getElementById("a2").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c2").innerHTML == turno) ||  // B|B|B|
+        (document.getElementById("a3").innerHTML == turno && document.getElementById("b3").innerHTML == turno && document.getElementById("c3").innerHTML == turno) ||  // C|C|C|
+        (document.getElementById("a1").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c3").innerHTML == turno) ||  // A\B\C\
+        (document.getElementById("a3").innerHTML == turno && document.getElementById("b2").innerHTML == turno && document.getElementById("c1").innerHTML == turno)     // C/B/A/
     ){
         document.getElementById(add).innerHTML = turno;
         showDialog('Tris', `Ha vinto il giocatore ${turno}`, 'Gioca Ancora');
